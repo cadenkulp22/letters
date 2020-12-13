c8c94b33cf73c9035bd56433fe44109f5e51e428
cadenkulp22 <54109914+cadenkulp22@users.noreply.github.com>
2020-12-13 14:34:18 -0700

    updates

--- drag_and_drop.js
+++ drag_and_drop.js
@@ -1,13 +1,19 @@
 function allowDrop(ev) {
-  ev.preventDefault();
+  ev.preventDefault();  // allows for elements to be dropped into other elements
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData("text", ev.target.id);  // sets the datatype and value ofthe dragged data
 }
 
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+
+  //ADDED to allow drag and copy, not just drag and drop
+  var nodeCopy =document.getElementById(data).cloneNode(true);
+  nodeCopy.id = "newId";
+  ev.target.appendChild(nodeCopy); // appends copied dragged data into drop element
+  // the following uncommented line is used for drag and drop
+  //ev.target.appendChild(document.getElementById(data)); // appends dragged data into drop element
 }
