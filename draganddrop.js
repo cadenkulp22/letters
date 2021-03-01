5de35a3de576ea0f2f43aaceaf925568f98631d1
cadenkulp22 <54109914+cadenkulp22@users.noreply.github.com>
2021-02-28 22:33:28 -0700

    FULLY FUNCTIONAL!!!

--- draganddrop.js
+++ draganddrop.js
@@ -22,18 +22,43 @@ function init() {
   //     revert: true
   //   } );
   // }
+  for ( var i=0; i<3; i++ ) {
+    $('<div class="row"></div>').attr( 'id', 'row'+(i+1) ).appendTo('#letterBank');
+  }
 
-  var letters = [ 'A', 'B', 'C', 'D' ];
+  var letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   // create spaces and actual letter blocks within space div
-  for ( var i=0; i<4; i++ ) {
-    $('<div class="spaces">' + letters[i] + '</div>').data( 'letter', letters[i] ).attr( 'id', 'space'+letters[i] ).appendTo('#letterBank').append(
-        $('<div>' + letters[i] + '</div>').data( 'letter', letters[i] ).addClass('letterBlocks').attr( 'id', 'letter'+letters[i] ).appendTo( '#letterBank' ).draggable( {
-          containment: '#content',
-          stack: '#letterBank div',
-          snap: true,
-          cursor: 'move',
-          revert: true
-        } ));
+  for ( var i=0; i<26; i++ ) {
+    if (i < 8) {    // first 8 letters: A, B, C, D, E, F, G, H
+      $('<div class="spaces">' + letters[i] + '</div>').data( 'letter', letters[i] ).attr( 'id', 'space'+letters[i] ).appendTo('#row1').append(
+          $('<div>' + letters[i] + '</div>').data( 'letter', letters[i] ).addClass('letterBlocks').attr( 'id', 'letter'+letters[i] ).appendTo( '#row1' ).draggable( {
+            containment: '#content',
+            stack: '#letterBank div',
+            snap: true,
+            cursor: 'move',
+            revert: true
+          } ));
+    }
+    else if ((i >= 8) && (i < 18)) {    // next 10 letters: I, J, K, L, M, N, O, P, Q, R
+      $('<div class="spaces">' + letters[i] + '</div>').data( 'letter', letters[i] ).attr( 'id', 'space'+letters[i] ).appendTo('#row2').append(
+          $('<div>' + letters[i] + '</div>').data( 'letter', letters[i] ).addClass('letterBlocks').attr( 'id', 'letter'+letters[i] ).appendTo( '#row2' ).draggable( {
+            containment: '#content',
+            stack: '#letterBank div',
+            snap: true,
+            cursor: 'move',
+            revert: true
+          } ));
+    }
+    else {    // last 8 letters: S, T, U, V, W, X, Y, Z
+      $('<div class="spaces">' + letters[i] + '</div>').data( 'letter', letters[i] ).attr( 'id', 'space'+letters[i] ).appendTo('#row3').append(
+          $('<div>' + letters[i] + '</div>').data( 'letter', letters[i] ).addClass('letterBlocks').attr( 'id', 'letter'+letters[i] ).appendTo( '#row3' ).draggable( {
+            containment: '#content',
+            stack: '#letterBank div',
+            snap: true,
+            cursor: 'move',
+            revert: true
+          } ));
+    }
   }
 
 
