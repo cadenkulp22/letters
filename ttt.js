2e6da3f0b47b3f093c27a6e4a57762de407909b0
cadenkulp22 <54109914+cadenkulp22@users.noreply.github.com>
2021-03-18 10:12:52 -0600

    logic works!!

--- ttt.js
+++ ttt.js
@@ -1,13 +1,16 @@
 // X = Team 1
 // O = Team 2
-var currTeam = 1;
+var currTeam = 'X';
+var boardSlots = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight' ];
+var currSlot;
+var currSlotNum = 0;
 
 $( init );
 
 function init() {
 
   $('#gameOver').hide();
-  $('#turn2').hide();
+  $('#turnO').hide();
 
   // Create the teams (X and O)
   var teams = [ 'X', 'O' ];
@@ -23,9 +26,9 @@ function init() {
   }
 
   // Create game board slots
-  var boardSpaces = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight' ];
+  // var boardSpaces = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight' ];
   for ( var i=0; i<9; i++ ) {
-    $('<div>' + boardSpaces[i] + '</div>').data( 'spaceNum', i ).addClass('board-space').appendTo( '.board' ).droppable( {
+    $('<div>' + boardSlots[i] + '</div>').data( 'slotNum', i ).addClass('board-slot').appendTo( '.board' ).droppable( {
       accept: '.team',
       hoverClass: 'hovered',
       drop: handleBlockDrop
@@ -39,7 +42,8 @@ function init() {
 }
 
 function handleBlockDrop(event, ui) {
-  var slotNumber = $(this).data( 'spaceNum' );
+  currSlot = $(this);
+  currSlotNum = $(this).data( 'slotNum' );
   var team = ui.draggable.data( 'team' );
   //createNewBlock(team);
 
@@ -53,29 +57,6 @@ function handleBlockDrop(event, ui) {
   // $(this).droppable( 'disable' );
   ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
   ui.draggable.draggable( 'option', 'revert', false );
-
-  $('#turnButton').click(function() {
-   $(this).data('clicked', true);
-  });
-
-  if($('#turnButton').data('clicked'))  {
-      // swap player turns
-      if (currTeam == 1) {
-        currTeam = 2;
-        $('#turn1').hide();
-        $('#turn2').show();
-        $(this).append(ui.draggable);
-        $('#teamX').draggable('disable');
-        createNewBlock('O');
-      }
-      else if (currTeam == 2) {
-        currTeam = 1;
-        $('#turn1').show();
-        $('#turn2').hide();
-        $('#teamO').draggable('disable');
-        createNewBlock('X');
-      }
-  }
 
 
 }
@@ -95,19 +76,44 @@ function createNewBlock(team) {
 function changeTurn() {
 
   // swap player turns
-  if (currTeam == 1) {
-    currTeam = 2;
+  if (currTeam === 'X') {
+    // solidify game board features
+    $('#teamX').draggable('disable');   // disable further dragging of current piece
+    var newID = 'placed' + currSlotNum;  // create new ID for piece
+    $('#teamX').attr('id',newID);   // change piece ID to placed in current slot
+    currSlot.droppable( 'disable' );  // disable dropping in the current slot
+    // store when item has been placed
+    boardSlots[currSlotNum] = 'X';
 
-    $('#turn1').hide();
-    $('#turn2').show();
-    $('.team').draggable('disable');
-    createNewBlock('O');
+    checkGameOver();
+
+    // change teams
+    currTeam = 'O';
+    createNewBlock(currTeam);   // create new block for O team
+    $('#turnX').hide();
+    $('#turnO').show();
   }
-  else if (currTeam == 2) {
-    currTeam = 1;
-    $('#turn1').show();
-    $('#turn2').hide();
-    $('#teamO').draggable('disable');
-    createNewBlock('X');
+  else if (currTeam === 'O') {
+    // solidify game board features
+    $('#teamO').draggable('disable');   // disable further dragging of current piece
+    var newID = 'placed' + currSlotNum;  // create new ID for piece
+    $('#teamO').attr('id',newID);   // change piece ID to placed in current slot
+    currSlot.droppable( 'disable' );  // disable dropping in the current slot
+    // store when item has been placed
+    boardSlots[currSlotNum] = 'O';
+
+    checkGameOver();
+
+    // change teams
+    currTeam = 'X';
+    createNewBlock(currTeam);   // create new block for X team
+    $('#turnO').hide();
+    $('#turnX').show();
+  }
+}
+
+function checkGameOver() {
+  if ( (boardSlots[0] === boardSlots[4]) && (boardSlots[4] === boardSlots[8]) ) {
+    $('#gameOver').show();
   }
 }
